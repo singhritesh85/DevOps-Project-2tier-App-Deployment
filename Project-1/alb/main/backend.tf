fc2cb932e09f610969a01932fbf5f2f8f1535506
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2024-05-03 11:14:07 +0530

    Update backend.tf

--- Project-1/alb/main/backend.tf
+++ Project-1/alb/main/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "dolo-dempo"
-    key = "alb/Jenkins-Master/dev/terraform.tfstate"
+    key = "alb/Jenkins-Master/dev/terraform.tfstate"  ### "alb/sonarqube/dev/terraform.tfstate"  for SonarQube  ### "alb/nexus/dev/terraform.tfstate" for Nexus
     region="us-east-2"
     dynamodb_table = "terraform-state"
   }
