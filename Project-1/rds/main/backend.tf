a7cf0efcc6f7272610d99ca8a2c8adb4fc748cd3
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2024-05-03 11:11:39 +0530

    Update backend.tf

--- Project-1/rds/main/backend.tf
+++ Project-1/rds/main/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "dolo-dempo"
-    key    = "rds/mysql/dev/terraform.tfstate"
+    key    = "rds/mysql/dev/terraform.tfstate"       ###  "rds/mysql/dev/terraform.tfstate"  for PostgreSQL
     region = "us-east-2"
     encrypt = true
     dynamodb_table = "terraform-state"
